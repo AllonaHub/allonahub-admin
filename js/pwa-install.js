4c609c5724635f2720d162d6ea054f5e2cdfff48
Allona Business <allonabusiness@192.168.1.2>
2026-06-26 13:13:45 +0300

    Remove homepage module label backing panel

--- js/pwa-install.js
+++ js/pwa-install.js
@@ -108,7 +108,7 @@
 
   if("serviceWorker" in navigator && (location.protocol === "https:" || location.hostname === "localhost" || location.hostname === "127.0.0.1")){
     window.addEventListener("load", () => {
-      navigator.serviceWorker.register("sw.js?v=20260626-mobile-std19", {scope: "./"})
+      navigator.serviceWorker.register("sw.js?v=20260626-mobile-std20", {scope: "./"})
         .then(registration => registration.update())
         .catch(() => undefined);
     });
