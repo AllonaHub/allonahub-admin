70b57c6470fa948e0c676b6bb1d1bd522b3c99f9
Allona Business <allonabusiness@192.168.1.2>
2026-06-26 05:30:01 +0300

    Mobil cache anahtarini yenile

--- js/pwa-install.js
+++ js/pwa-install.js
@@ -108,7 +108,7 @@
 
   if("serviceWorker" in navigator && (location.protocol === "https:" || location.hostname === "localhost" || location.hostname === "127.0.0.1")){
     window.addEventListener("load", () => {
-      navigator.serviceWorker.register("sw-reset2.js?v=20260626-mobile-std1", {scope: "./"})
+      navigator.serviceWorker.register("sw-reset2.js?v=20260626-mobile-std2", {scope: "./"})
         .then(registration => registration.update())
         .catch(() => undefined);
     });
