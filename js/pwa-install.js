49b5d73c6844d549a37766a826b3d51926c38a4c
Allona Business <allonabusiness@192.168.1.2>
2026-06-26 14:45:54 +0300

    Restore pre-ellipsis module label style

--- js/pwa-install.js
+++ js/pwa-install.js
@@ -108,7 +108,7 @@
 
   if("serviceWorker" in navigator && (location.protocol === "https:" || location.hostname === "localhost" || location.hostname === "127.0.0.1")){
     window.addEventListener("load", () => {
-      navigator.serviceWorker.register("sw.js?v=20260626-mobile-std26", {scope: "./"})
+      navigator.serviceWorker.register("sw.js?v=20260626-mobile-std27", {scope: "./"})
         .then(registration => registration.update())
         .catch(() => undefined);
     });
