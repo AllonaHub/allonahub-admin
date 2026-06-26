ff109d6d8ae73f1945d26f67298bff357100cf4b
Allona Business <allonabusiness@192.168.1.2>
2026-06-26 05:38:51 +0300

    Mobil link hedeflerini standartlastir

--- js/pwa-install.js
+++ js/pwa-install.js
@@ -108,7 +108,7 @@
 
   if("serviceWorker" in navigator && (location.protocol === "https:" || location.hostname === "localhost" || location.hostname === "127.0.0.1")){
     window.addEventListener("load", () => {
-      navigator.serviceWorker.register("sw.js?v=20260626-mobile-std4", {scope: "./"})
+      navigator.serviceWorker.register("sw.js?v=20260626-hero-customads1", {scope: "./"})
         .then(registration => registration.update())
         .catch(() => undefined);
     });
